3ced5b1f6e83784fffc9801e4c2c32a133bf52db
Michael Scofield <mscofield0@tutanota.com>
2024-12-26 20:34:02 +0100

    remove need for `locale` as every type will use its static crate one

--- src/lib.rs
+++ src/lib.rs
@@ -4,39 +4,16 @@
 ///
 /// [`fluent_templates::Loader`]: https://docs.rs/fluent-templates/latest/fluent_templates/trait.Loader.html
 pub trait LocalizedDisplay {
-    fn localize(
-        &self,
-        lang: &fluent_templates::LanguageIdentifier,
-        locale: &dyn fluent_templates::Loader,
-    ) -> String;
+    fn localize(&self, lang: &fluent_templates::LanguageIdentifier) -> String;
 }
 
 // #[cfg(tests)]
 mod tests {
     use super::*;
+    use fluent_templates::Loader;
 
     #[test]
     fn test_if_type_works() {
-        enum Foo {
-            A,
-            B,
-            C,
-        }
-
-        impl LocalizedDisplay for Foo {
-            fn localize(
-                &self,
-                lang: &fluent_templates::LanguageIdentifier,
-                locale: &dyn fluent_templates::Loader,
-            ) -> String {
-                match self {
-                    Self::A => locale.lookup(lang, "foo-a"),
-                    Self::B => locale.lookup(lang, "foo-b"),
-                    Self::C => locale.lookup(lang, "foo-c"),
-                }
-            }
-        }
-
         fluent_templates::static_loader! {
             pub static LOCALE = {
                 locales: "i18n",
@@ -44,13 +21,29 @@ mod tests {
             };
         }
 
+        enum Foo {
+            A,
+            B,
+            C,
+        }
+
+        impl LocalizedDisplay for Foo {
+            fn localize(&self, lang: &fluent_templates::LanguageIdentifier) -> String {
+                match self {
+                    Self::A => LOCALE.lookup(lang, "foo-a"),
+                    Self::B => LOCALE.lookup(lang, "foo-b"),
+                    Self::C => LOCALE.lookup(lang, "foo-c"),
+                }
+            }
+        }
+
         let a = Foo::A;
         let b = Foo::B;
 
-        let res = a.localize(&fluent_templates::langid!("en-US"), &*LOCALE);
+        let res = a.localize(&fluent_templates::langid!("en-US"));
         assert_eq!(res, "English A");
 
-        let res = a.localize(&fluent_templates::langid!("hr-hr"), &*LOCALE);
+        let res = a.localize(&fluent_templates::langid!("hr-hr"));
         assert_eq!(res, "Croatian A");
     }
 }
